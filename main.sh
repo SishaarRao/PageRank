a0a2d5cac990d09dd1f58af051b8e267ff25a529
Sishaar Rao <sishaar@gmail.com>
2017-05-04 13:53:43 -0400

    Print function and construction of matrix

--- main.sh
+++ main.sh
@@ -1,11 +1,36 @@
-#usr/bin/bash
+#!/bin/bash
 
+count=`ls Pages/ | wc -l`
 
-# Count the number of files
-$numOfFiles = `ls Pages/ | wc -l`
+# Function for printing out the matrix
+function printMatrix() {
+    f1="%$((${#count}+1))s"
+    f2=" %9s"
+    
+    printf "$f1" ''
+    for ((i=1;i<=$count;i++)); do
+	printf "$f2" $i
+    done
+    echo
 
-# Construct matrix
+    for ((j=1;j<=$count;j++)); do
+	printf "$f1" $j
+	for ((i=1;i<=$count;i++)); do
+	    printf "$f2" ${matrix[$i,$j]}
+	done
+	echo
+    done
+    
+}
 
+# Construct the Matrix
+for ((i=1;i<=$count;i++)) do
+    for ((j=1;j<=$count;j++)) do
+        matrix[$i,$j]=0
+    done
+done
+    
+printMatrix
 
 #Parse text, filling in the matrix
 
