659cc5d49c67808bd6572391e04d01f679a7cee0
Sishaar Rao <sishaar@gmail.com>
2017-05-04 13:27:34 -0400

    basic outline for what to do

---
+++ main.sh
@@ -0,0 +1,16 @@
+#usr/bin/bash
+
+
+# Count the number of files
+$numOfFiles = `ls Pages/ | wc -l`
+
+# Construct matrix
+
+
+#Parse text, filling in the matrix
+
+
+# Matrix calculations
+
+
+# Output Rank
