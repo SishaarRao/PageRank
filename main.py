96fc1b4713296b7bb0b416f6c658aeeef0e545ac
Sishaar Rao <sishaar@gmail.com>
2017-05-09 18:13:43 -0400

    Finished outputing the eigenvector

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 #!/bin/env python3
 
 import numpy as np
+from scipy.sparse.linalg import eigs
 import os
+import time
 
-def main():
+
+def main():    
     count = int(os.popen("ls Pages/ | wc -l").read().strip())
-    matrix = [[0]*count]*count
-    print(matrix)
-    
-    
+    matrix = [x[:] for x in [[0]*count]*count]
+    parserOutput = os.popen("./parser.sh").read().split()
+    i = 0
+    while i < len(parserOutput):
+        matrix[int(parserOutput[i])][int(parserOutput[i+1])] = float(parserOutput[i+2])
+        i += 3
+    print("Constructed Matrix:\n" + str(np.matrix(matrix)) + "\n\n")
+    vals, vecs = eigs(np.array(matrix), k=1)
+    print("Eigenvalues:\n" + str(np.matrix(vals)) + "\n\n")
+    print("Eigenvectors:\n"+ str(np.matrix(vecs)) + "\n\n")
 
 main()
+
+
