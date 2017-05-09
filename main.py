0085fa492fb31d666170fdbfa912458ded39e5a8
Sishaar Rao <sishaar@gmail.com>
2017-05-09 18:16:31 -0400

    added comments for greater readability when presenting

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import time
 
 
 def main():    
+    # Figure out how many files there are to read, construct matrix with that size
     count = int(os.popen("ls Pages/ | wc -l").read().strip())
     matrix = [x[:] for x in [[0]*count]*count]
+
+    # Parse the text files and populate matrix with data from Parser
     parserOutput = os.popen("./parser.sh").read().split()
     i = 0
     while i < len(parserOutput):
         matrix[int(parserOutput[i])][int(parserOutput[i+1])] = float(parserOutput[i+2])
         i += 3
+        
+    # Output Data while calculating the Eigenvectors
     print("Constructed Matrix:\n" + str(np.matrix(matrix)) + "\n\n")
     vals, vecs = eigs(np.array(matrix), k=1)
     print("Eigenvalues:\n" + str(np.matrix(vals)) + "\n\n")
