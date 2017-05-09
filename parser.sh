25cd32f0326572120529a3ab844c151b80ba6b19
Sishaar Rao <sishaar@gmail.com>
2017-05-09 18:13:55 -0400

    Finished parser that reads through HTML files

--- parser.sh
+++ parser.sh
@@ -4,13 +4,10 @@ for page in `ls Pages/*.html`; do
     denom=`grep -c "link:to:*" $page`
     links=`grep "link:to:*" $page`
     col=${page:10:1}
-    echo $denom
-    echo
     for link in $links; do
 	row=${link:12:1}
-	echo $row
-	echo $col
-	#matrix[$row,$col]=$(echo "scale=6; 1/$denom" |bc)
-	matrix[$row,$col]=1
+	echo `expr $row - 1`
+	echo `expr $col - 1`
+	echo $(echo "scale=6; 1/$denom" | bc)
     done
 done
